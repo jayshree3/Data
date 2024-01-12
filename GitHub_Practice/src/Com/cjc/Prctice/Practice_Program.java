d3d683894ca4eec720468fae0f890c3d9d2bab51
HP <HP@LAPTOP-KUH9GEJ5>
2024-01-12 13:32:12 +0530

    By Jayshree

--- GitHub_Practice/src/Com/cjc/Prctice/Practice_Program.java
+++ GitHub_Practice/src/Com/cjc/Prctice/Practice_Program.java
@@ -6,6 +6,11 @@ public class Practice_Program
 	{
 		System.out.println("Jayshree");
 	}
+	public void m2()
+	{
+		System.out.println("Karande");
+	}
+			 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome CJC");
