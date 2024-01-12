33284639d38201bf03217ca6025e1c29e140a38f
jayshree3 <156280181+jayshree3@users.noreply.github.com>
2024-01-12 15:52:14 +0530

    xxxx

--- GitHub_Practice/src/Com/cjc/Prctice/Practice_Program.java
+++ GitHub_Practice/src/Com/cjc/Prctice/Practice_Program.java
@@ -2,14 +2,7 @@ package Com.cjc.Prctice;
 
 public class Practice_Program 
 {
-	public void m1()
-	{
-		System.out.println("Jayshree");
-	}
-	public void m2()
-	{
-		System.out.println("Karande");
-	}
+	
 	public Branch()
 	{
          System.out.println("Branch---");
